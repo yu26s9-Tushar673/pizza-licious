038b80b13c855ba9ee41f0b1c9dae83489b721a9
Tushar Islam <tushar060703@gmail.com>
2026-05-25 15:17:44 -0400

    OrderItem extended class - Pizza Implemented

---
+++ src/main/java/com/pluralsight/orderItems/GarlicKnots.java
@@ -0,0 +1,4 @@
+package com.pluralsight.orderItems;
+
+public class GarlicKnots {
+}
---
+++ src/main/java/com/pluralsight/orderItems/Pizza.java
@@ -0,0 +1,60 @@
+package com.pluralsight.orderItems;
+
+import com.pluralsight.abstracts.OrderItem;
+import com.pluralsight.abstracts.Topping;
+import com.pluralsight.enums.CrustType;
+import com.pluralsight.enums.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza extends OrderItem {
+    private Size size;
+    private CrustType crustType;
+    private List<Topping> toppings;
+    private boolean isStuffedCrust;
+
+    public Pizza(Size size, CrustType crust, boolean isStuffedCrust) {
+        super(1);
+        this.size = size;
+        this.crustType = crust;
+        this.isStuffedCrust = isStuffedCrust;
+        this.toppings = new ArrayList<>();
+    }
+
+    public void addToppings(Topping topping) {    toppings.add(topping);    }
+
+    @Override
+    public double getPrice() {
+        double total = size.getBasePrice();
+        if (isStuffedCrust) {
+            total += switch (size) {
+                case PERSONAL -> 1.00;
+                case MEDIUM -> 1.50;
+                case LARGE -> 2.00;
+            };
+        }
+
+        for (Topping t : toppings){
+            total += t.getPrice(size);
+        }
+        return total;
+    }
+
+    @Override
+    public String getDescription() {
+        StringBuilder order = new StringBuilder();
+        order.append(String.format("Pizza - %s, %s, crust%s - $%.2f%n",
+                size, crustType, isStuffedCrust ? ", Stuffed" : "", getPrice()));
+        for (Topping t: toppings) {
+            order.append(String.format("   + %-25s $%.2f%n", t.getDescription(), t.getPrice(size)));
+        }
+        return order.toString();
+    }
+
+    // Getters
+    public Size getSize() {    return size;    }
+    public CrustType getCrustType() {   return crustType;   }
+    public List<Topping> getToppings() {    return toppings;    }
+    public boolean isStuffedCrust() {   return isStuffedCrust;  }
+}
