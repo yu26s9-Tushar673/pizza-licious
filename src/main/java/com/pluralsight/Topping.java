338a4157e6c82c48ac711c0dc602ff86c19bb59f
Tushar Islam <tushar060703@gmail.com>
2026-05-25 14:13:33 -0400

    Updated Abstract Topping class

--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -1,5 +1,23 @@
 package com.pluralsight;
 
+import com.pluralsight.enums.Size;
+
 public abstract class Topping {
-    
+    protected String name;
+    protected boolean isExtra;
+
+    // Constructor
+    public Topping(String name, boolean isExtra) {
+        this.name = name;
+        this.isExtra = isExtra;
+    }
+
+    // Abstract Methods
+    public abstract double getPrice(Size size);
+    public abstract String getDescription();
+
+    // Getter and Setters
+    public String getName() {   return name;   }
+    public boolean isExtra() {   return isExtra;   }
+    public void setExtra(boolean extra) {   isExtra = extra;   }
 }
