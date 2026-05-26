c8a0b068b5e6805882a5c4d9d193d04d1ca1f419
Tushar Islam <tushar060703@gmail.com>
2026-05-26 12:54:45 -0400

    Order Items extended classes completed (GarlicKnots, Drink)

--- src/main/java/com/pluralsight/orderItems/Drink.java
+++ src/main/java/com/pluralsight/orderItems/Drink.java
@@ -1,4 +1,27 @@
 package com.pluralsight.orderItems;
 
-public class Drink {
+import com.pluralsight.abstracts.OrderItem;
+import com.pluralsight.enums.DrinkSize;
+
+public class Drink extends OrderItem {
+    private DrinkSize drinkSize;
+    private String flavor;
+
+    public Drink(DrinkSize size, String flavor) {
+        super(1);
+        this.drinkSize = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {     return drinkSize.getPrice();    }
+
+    @Override
+    public String getDescription() {
+        return String.format("Drink - %s, %s - $%.2f", drinkSize, flavor, drinkSize.getPrice());
+    }
+
+    // Getters
+    public DrinkSize getDrinkSize() {   return drinkSize;   }
+    public String getFlavor() {  return flavor;  }
 }
--- src/main/java/com/pluralsight/orderItems/GarlicKnots.java
+++ src/main/java/com/pluralsight/orderItems/GarlicKnots.java
@@ -1,4 +1,21 @@
 package com.pluralsight.orderItems;
 
-public class GarlicKnots {
+import com.pluralsight.abstracts.OrderItem;
+
+public class GarlicKnots extends OrderItem {
+    private static final double price = 1.50;
+
+    public GarlicKnots(int quantity) {
+        super(quantity);
+    }
+
+    @Override
+    public double getPrice() {
+        return quantity * price;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("Garlic Knots - x%d - $%.2f", quantity, getPrice());
+    }
 }
